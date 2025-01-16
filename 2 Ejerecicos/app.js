3407bac9315075f765cec3586feb5a5c50905a7a
Jack1812 <jackandypabloalvarado@gmail.com>
2025-01-16 16:35:15 -0500

    Los ejerecicisos de inicio de clases

---
+++ 2 Ejerecicos/app.js
@@ -0,0 +1,33 @@
+
+1.
+//alert('¡Bienvenido a nuestro sitio web!');
+2.
+
+// let nombre = "Lua";
+3.
+
+// let edad = 25;
+4.
+
+// let numeroDeVentas = 50;
+5.
+
+// let saldoDisponible = 1000;
+6.//"¡Error! Completa todos los campos."
+
+// alert('¡Error! Preencha todos los campos');
+7.//"¡Error! Preencha todos los campos." 
+
+// let mensajeDeError = '¡Error! Preencha todos los campos';
+// alert(mensajeDeError);
+8.
+
+// let nombre = prompt('Digite su nombre');
+9.
+
+// let edad = prompt('Digite su edad');
+10.
+// let edad = prompt('Digite su edad');
+// if (edad >= 18) {
+   // alert('¡Puede obtener su licencia de conducir!');
+// }
