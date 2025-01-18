5591f86e988aec33714d509b10464791983d9755
Jack1812 <jackandypabloalvarado@gmail.com>
2025-01-17 20:11:43 -0500

    ejerecicio

--- 1/app.js
+++ 1/app.js
@@ -1,8 +1,11 @@
-let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero por favor:");
+let numeroSecreto = 9;
+let numeroUsuario = prompt("Me indicas un numero del 1 al 10por favor:");
 
 console.log(numeroUsuario);
 
 if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el numero');
+    alert(`Acertaste el numero es ${numeroUsuario}`);
+}
+else{
+    alert('No asertastes el numero')
 }
