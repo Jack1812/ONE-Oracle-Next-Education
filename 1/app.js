d19670351c36c6d700d2ce96125742abb564b998
Jack1812 <jackandypabloalvarado@gmail.com>
2025-01-29 14:59:03 -0500

    Update app.js

--- 1/app.js
+++ 1/app.js
@@ -1,15 +1,18 @@
-let numeroSecreto = 5;
+// Variables 
+let numeroMaximo = 100;
+let numeroSecreto = Math.floor(Math.random() * numeroMaximo) + 1;
 let numeroUsuario = 0;
 let intentos=1;
-let palabras='vez';
+// let palabras='vez';
+let maximosIntentos =3;
 
 while (numeroUsuario != numeroSecreto) {
-    numeroUsuario = parseInt(prompt("Me indicas un numero del 1 al 10 por favor:"));
+    numeroUsuario = parseInt(prompt(`Me indicas un numero del 1 al ${numeroMaximo} por favor:`));	
 
     console.log(numeroUsuario);
 
     if (numeroUsuario == numeroSecreto) {
-        alert(`Acertaste el numero, es ${numeroUsuario}. Lo hiciste en ${intentos} ${palabras} `);
+        alert(`Acertaste el numero, es ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'} `);
     } else {
         if (numeroUsuario > numeroSecreto) {
             alert('El numero secreto es menor');
@@ -17,6 +20,10 @@ while (numeroUsuario != numeroSecreto) {
             alert('El numero secreto es mayor');
         }
         intentos = intentos + 1;
-        palabras = 'veces';
+        palabrasVeces = 'veces';
+        if (intentos > 3){
+            alert(`Llegaste al número maximo de ${maximosIntentos} intentos`);
+            break;
+        }
     }
 }
